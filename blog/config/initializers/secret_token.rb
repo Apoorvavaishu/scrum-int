66df38a9d091b5f6295f8765c8b63e69625fd29d
Apoorva Tare <apoorvatare@gmail.com>
2013-09-09 01:06:40 -0700

    removed readme

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '32c08fbd3230ea3644bb69b1ced2536edb582447c4263129206c4fe69e062bc4afc0f97caf73c5f6f6b451b8cfed7e06bc1529494f3671e0f88315a0e72fb623'
